bbf5093544c846789d977ac691ab079a7d29d40b
Leandro Hanc <leandrohanc@gmail.com>
2023-07-04 22:23:31 -0300

    Use FastifyAdapter. Update nestjs.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,17 @@
-import { Logger } from "@nestjs/common";
+import { Logger, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter()
+	);
 
 	/* App config */
 	const port = process.env.PORT || 5555;
@@ -12,8 +19,11 @@ async function bootstrap() {
 	/* API config */
 	const globalPrefix = "api";
 	app.setGlobalPrefix(globalPrefix);
+	app.enableVersioning({
+		type: VersioningType.URI,
+	});
 
-	await app.listen(port);
+	await app.listen(port, "0.0.0.0");
 	Logger.log(
 		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
 	);
