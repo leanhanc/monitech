46d11ec7402e04762b04c153527f15890c7eab9c
Leandro Hanc <leandrohanc@gmail.com>
2023-08-06 11:10:15 -0300

    Smaller border for currency input

--- apps/frontend/app/invoice/new/components/NewInvoiceFormAmountStep.tsx
+++ apps/frontend/app/invoice/new/components/NewInvoiceFormAmountStep.tsx
@@ -28,7 +28,7 @@ export default function NewInvoiceFormAmountStep({
 				allowNegativeValue={false}
 				decimalSeparator=","
 				groupSeparator="."
-				className="h-[132px] w-[280px] max-w-full rounded-lg border-2 border-indigo-600 bg-indigo-50 p-6 text-center text-3xl font-bold text-slate-700 placeholder:text-3xl placeholder:text-slate-700 placeholder:opacity-30 md:w-auto md:text-5xl md:placeholder:text-5xl lg:text-7xl lg:placeholder:text-7xl"
+				className="h-[132px] w-[280px] max-w-full rounded-lg border border-indigo-600 bg-indigo-50 p-6 text-center text-3xl font-bold text-slate-700 placeholder:text-3xl placeholder:text-slate-700 placeholder:opacity-30 md:w-auto md:text-5xl md:placeholder:text-5xl lg:text-7xl lg:placeholder:text-7xl"
 			/>
 		</p>
 	);
