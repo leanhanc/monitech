a060533c8e703bd37aea3bf791a689d614d8f323
Leandro Hanc <leandrohanc@gmail.com>
2023-08-28 19:52:05 -0300

    Component refactor

--- apps/frontend/app/invoices/new/page.tsx
+++ apps/frontend/app/invoices/new/page.tsx
@@ -1,13 +1,43 @@
-/* View */
-import NewInvoiceView from "@frontend/invoices/new/view";
+import NextLink from "next/link";
 
 /* Components */
 import Container from "@frontend/components/Container";
+import Typography from "@frontend/components/Typography";
+
+/* Utils */
+import ROUTES from "@frontend/lib/utils/routes";
 
 export default async function NewInvoicePage() {
 	return (
 		<Container className="flex min-h-full flex-col" as="main">
-			<NewInvoiceView />
+			<header>
+				<Typography.Title variant="main" as="h1">
+					Nueva Factura 🧾
+				</Typography.Title>
+				<Typography.Paragraph variant="long-subheading" as="p" className="mt-6">
+					Elegí el tipo de factura que vas a cargar
+				</Typography.Paragraph>
+			</header>
+			<ul className="mt-12 flex w-1/2 gap-2">
+				<li>
+					<NextLink
+						href={ROUTES.INVOICES.NEW.C}
+						className="flex flex-col items-center justify-center gap-1 rounded-md border-2 border-slate-500 bg-slate-50 p-2 hover:border-indigo-700 hover:text-indigo-700 hover:shadow-md"
+					>
+						<span>FACTURA</span>
+						<strong className="text-4xl font-bold">C</strong>
+					</NextLink>
+				</li>
+				<li>
+					<NextLink
+						href={ROUTES.INVOICES.NEW.E}
+						className="flex flex-col items-center justify-center gap-1 rounded-md border-2 border-slate-500 bg-slate-50 p-2 hover:border-indigo-700 hover:text-indigo-700 hover:shadow-md"
+					>
+						<span>FACTURA</span>
+						<strong className="text-4xl font-bold">E</strong>
+					</NextLink>
+				</li>
+			</ul>
 		</Container>
 	);
 }
